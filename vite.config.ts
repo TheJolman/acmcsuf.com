ed45e2c1c455dd96216a4234124ed48d1a927699
josh <joshuaholman5@gmail.com>
2025-09-25 00:10:14 -0700

    rm cloudflare vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import { cloudflare } from "@cloudflare/vite-plugin"
 
 export default defineConfig({
-  plugins: [sveltekit(), cloudflare()],
+  plugins: [sveltekit()],
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}'],
   },
